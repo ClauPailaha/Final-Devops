08f63ca687bbad7792d7eaf9e11e4574c1f076ed
ClauPailaha <s22110150@student.unklab.ac.id>
2024-11-25 12:05:38 +0800

    foto

--- index.php
+++ index.php
@@ -1,13 +1,3 @@
-<!-- Aplikasi CRUD dengan PHP 8, MySQL 8, Bootstrap 5, dan jQuery AJAX
-***********************************************************************
-* Developer   : Indra Styawantoro
-* Company     : Pustaka Koding
-* Release     : September 2023
-* Update      : -
-* Website     : pustakakoding.com
-* E-mail      : pustaka.koding@gmail.com
-* WhatsApp    : +62-813-7778-3334
--->
 
 <?php
 // panggil file "database.php" untuk koneksi ke database
