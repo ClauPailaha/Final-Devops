0926510f4f836396ec2571ed732ea3b0fd2b9005
ClauPailaha <s22110150@student.unklab.ac.id>
2024-11-25 11:15:03 +0800

    fix db

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 // deklarasi parameter koneksi database
 $sql_details = array(
     'user' => 'root',                               // username database, default “root”
-    'pass' => '',                                   // password database, default kosong
+    'pass' => 'Devops_needs11',                                   // password database, default kosong
     'db'   => 'db_crud_jquery_ajax',                // nama database yang akan digunakan
-    'host' => 'localhost'                           // server database, default “localhost” atau “127.0.0.1”
+    'host' => '103.217.145.115'                           // server database, default “localhost” atau “127.0.0.1”
 );
